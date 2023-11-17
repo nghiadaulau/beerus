532a719937c297243caec5d779584e9dcfb8a56f
nghiadaulau <nhatnghiatyper@gmail.com>
2023-11-17 23:20:26 +0700

    show database

--- Car.cs
+++ Car.cs
@@ -8,10 +8,15 @@ namespace beerus
 {
     public class Car
     {
+        public int car_id { get; set; }
         public string name { get; set; }
         public string brand { get; set; }
         public string model { get; set; }
         public string car_type { get; set; }
-        
+
+        public override string ToString()
+        {
+            return this.name;
+        }
     }
 }
